71171e023edb50058cbb79e95799b67384038002
nadjs-1702 <148224731+nadjs-1702@users.noreply.github.com>
2025-06-13 15:56:51 +0200

    alles no dööfer idk will doch nur resete

--- add_food_field.sql
+++
@@ -1,7 +0,0 @@
--- Add food field to care table
-ALTER TABLE `care` 
-ADD COLUMN `food` varchar(255) DEFAULT NULL AFTER `medication_frequency`,
-ADD COLUMN `food_frequency` int(11) DEFAULT NULL AFTER `food`;
-
--- Update existing records to have NULL values for new fields
-UPDATE `care` SET `food` = NULL, `food_frequency` = NULL; 
